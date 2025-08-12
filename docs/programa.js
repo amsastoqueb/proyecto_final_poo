cc697886aeaf7a4855ae5588508b0a9053a662f5
angieoosrio <angieoosrio@gmail.com>
2025-08-11 21:29:26 -0500

    v2

--- docs/programa.js
+++ docs/programa.js
@@ -1,6 +1,3 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const mapa = document.getElementById("mapa");
-    if (mapa) {
-        mapa.innerHTML = "<p style='text-align:center; padding-top:200px;'>Aquí se cargará el mapa interactivo</p>";
-    }
+    console.log("Página cargada correctamente.");
 });
